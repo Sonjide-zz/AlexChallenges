4a2719b1b552fef7df4ce259eb6e18ba20b84321
Sonjide Hussain <sonjide.hussain@gmail.com>
2021-03-28 00:24:49 +0000

    saving all loop examples

--- app.js
+++ app.js
@@ -1,2 +1,116 @@
 console.log("loopy")
 
+let figures = [2, 4, 6, 8, 9];
+let words = ["cat", "dog", "avocado", "boat", "goat"];
+let chessboardLength = 8;
+
+console.log("I'm going to start a for loop!");
+
+// let doubleFigures = [];
+// for (let i = 0; i < figures.length; i++) {
+//   let startingNum = figures[i];
+//   let doubleStartingNum = startingNum*2;
+//   let squareStartingNum = startingNum**2;
+//   doubleFigures.push(doubleStartingNum);
+
+//   console.log(`At index ${i}, we have ${startingNum}. Its double is ${doubleStartingNum} and its square is ${squareStartingNum}.`);
+// }
+
+// console.log("All done");
+// console.log(`My doubleFigures is now ${doubleFigures}`)
+
+/* Let's write two separate for loops.
+   1. Prints out all the words in uppercase. DONE
+   2. Builds a new array of just the first two letters in each word. ("ca", "do", "av", "bo", "go".) DONE
+ */
+
+/*
+for (i = 0; i < words.length; i++)
+{
+  let upperCaseWords = words[i].toUpperCase()
+
+  console.log(upperCaseWords)
+//return upperCaseWords (why did this now work)
+}
+//return upperCaseWords (why did this now work)
+*/
+
+// let testWord = ["cat", "dog"];
+
+// let newWrd = testWord.substring(0, 2)
+
+// console.log(testWord.substring(0, 2))
+
+
+// let newArray = [];
+// for (i = 0; i < words.length; i++)
+// {
+// //let twoLetters = words[i].slice(0,2)
+// let twoLetters = words[i].substring(0,2)
+
+// //console.log(words[i].slice(0,2));
+// newArray.push(twoLetters)
+// }
+// console.log(newArray)
+
+//Q i dont understand where to put console.log() and where to add methods.trim() for example.
+
+//console.log(newArray.replace(" ", "")) or stringName.trim()
+
+
+/* Write a new for loop, using chessboardLength.
+   1. Print out a row that's as long as specified.
+      If it's a black square, print "X"
+      If it's a white square, print "."
+      So for an 8-long row, print ".X.X.X.X"
+   2. A real stretch goal: put this for loop in another for
+      loop. And print out a row for everything. So, if
+      chessboardLength = 4, print:
+      ".X.X
+       X.X.
+       .X.X
+       X.X."
+       */
+
+//let black = "x";
+//let white = ".";
+//console.log(black)
+
+// let numbr = 3;
+
+// if(numbr%2 === 0){
+//   console.log("even")
+// }
+// else if(numbr%1 ===)
+// console.log("odd")
+// }
+
+let chessboardLength = 8;
+
+for (let i = 0; 0 < chessboardLength.length; i++){
+
+newChessboard = chessboardLength[i]
+
+}
+
+
+
+
+
+
+
+
+// let numbArray = [1,8,5,9,0]
+// let orderedArray =[]
+
+//       for (i =0; i < numbArray.length; i++)
+//       {
+//         let orderArray = numbArray[i]
+// console.log(numbArray)
+//       }
+
+
+
+     // sort orginal array 0, 1, 5, 8, 9
+
+
---
+++ loops.js
@@ -0,0 +1,48 @@
+console.log("loopy")
+
+
+//normal for loop
+// for ([initialiasaition]; [condition]; [final-expression(incrementation or decrementation) (interate the counter)]){
+//   "something happens here"
+// }
+//for loop
+let array = [10,20,30]
+for (let i=0; i < array.length; i++ ){
+  //console.log(array[i])
+}
+
+//multi-dimensional array, allows you to use nested for loops to go through sub-arrays
+//var arrayNestArr =[[1,3],[4,7],[9,8]]
+let arrayNestArr =[[1,3],[4,7],[9,8]]
+for(let i=0; i < arrayNestArr.length; i++){
+for(let k=0; k < arrayNestArr[i].length; k++){
+//console.log(arrayNestArr[[i],[k]]);
+//console.log(arrayNestArr[[i]?[k]]); why did i get undefined space where the ? is counts as space that is why output is undefined
+// console.log(arrayNestArr[[i , k]]); all are undefined ... now I am not sure TBH
+
+//console.log(arrayNestArr[i][k]);
+}
+}
+
+//seperate the even and odd numbers
+let number = 8;
+let x = [];
+let y =[];
+let evenOdd = [[x],[y]];
+
+
+for (let i = 0; i < number.length; i++){
+  if (i %2 === 0){
+     console.log(x.push(i));
+  }
+}
+console.log(x)
+
+
+// for (let i = 0; i < number.length; i++){
+//   for (let j = 0; j < number[i].length; j++){
+// console.log(x.push(i));
+// console.log(y.push(j));
+//   }
+// }
+//   console.log(evenOdd)
